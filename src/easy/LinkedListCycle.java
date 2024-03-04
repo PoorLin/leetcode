8b0311e296ae645f6d4df3e7b597c57d479cf9d6
B02\User <s80532s@gmail.com>
2024-03-04 14:15:19 +0800

    <easy> 題目:isSubsequence  runtime : 0ms beats 100%  memory : 44.73MB beats 14.87%
    important : slow fast pointer 演算法
    
    前提是因為這個是存node，一個記憶體位址才可以這樣

---
+++ src/easy/LinkedListCycle.java
@@ -0,0 +1,45 @@
+package easy;
+
+
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+         val = x;
+          next = null;
+      }
+ }
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+        ListNode head1=new ListNode(3);
+        ListNode head2=new ListNode(2);
+        ListNode head3=new ListNode(0);
+        ListNode head4=new ListNode(-4);
+head1.next=head2;
+head2.next=head3;
+head3.next=head4;
+head4.next=head1;
+        hasCycle(head1);
+
+    }
+    public static boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+
+        ListNode fast = head;
+        while (fast!=null && fast.next!=null){
+
+            slow =slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+
+                return true;
+
+            }
+
+        }
+
+        return false;
+    }
+}
