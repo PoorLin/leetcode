5daa9b4e39e699b7175f7784869d01f6cb4de5c5
jack <s80532s@gmail.com>
2024-04-05 14:45:13 +0800

    <easy> 題目:MinimumAbsoluteDifferenceinBST 修正前 runtime : Runtime 494 ms Beats 5% ->  runtime : Runtime 2 ms Beats 20.51%  memory :  44.3MB beats 55%

--- src/easy/MinimumAbsoluteDifferenceinBST.java
+++ src/easy/MinimumAbsoluteDifferenceinBST.java
@@ -1,12 +1,63 @@
 package easy;
 
+import java.util.*;
+
 public class MinimumAbsoluteDifferenceinBST {
+    int mnd = 0;
+    TreeNode pn;
     public int getMinimumDifference(TreeNode root) {
+        //先往最左邊走
+       List<Integer> list =new ArrayList<>();
 
-        //root 若是往左比,到其下一層就不需要再向左比了
+        Map<Integer,Integer> map =new HashMap<>();
+        map.put(0,100001);
+        getLeft(root,list);
+int listLen=list.size();
+   for(int test = 0;test<listLen;test++){
+       System.out.println(list.get(test));
+   }
+
+     for(int index = 0;index<listLen-1;index++){
+
+              int res=Math.abs(list.get(index)-list.get(index+1));
+              if(map.get(0)>res){
+                  map.put(0,res);
+              }
+          }
+        return map.get(0);
+     }
 
 
 
-return 123;
+    public static void getLeft(TreeNode root, List<Integer> list){
+       if(root.left!=null && root.right!=null){
+           getLeft(root.left,list);
+           list.add(root.val);
+           getLeft(root.right,list);
+       }else if(root.left!=null){
+           getLeft(root.left,list);
+           list.add(root.val);
+
+       }else if(root.right!=null){
+           list.add(root.val);
+           getLeft(root.right,list);
+       }else {
+           list.add(root.val);
+       }
     }
+    public int getMinimumDifferenceBetter(TreeNode root) {
+        mnd = Integer.MAX_VALUE;
+        pn = null;
+        helper(root);
+        return mnd;
+    }
+    private void helper(TreeNode n) {
+        if( n == null ) return;
+        helper(n.left);
+        if( pn != null )
+            mnd = Math.min(mnd, Math.abs(pn.val-n.val));
+        pn = n;
+        helper(n.right);
+    }
+
 }
