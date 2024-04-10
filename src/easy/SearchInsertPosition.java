6d16cf899cbbb5d4dc222fe6d215794924eedb95
jack <s80532s@gmail.com>
2024-04-10 21:15:39 +0800

    <modify> 刪掉sout 題目:searchInsert 修正前 runtime : Runtime 0 ms Beats 100% 修正前 memory :  42.58MB beats 86.24%

--- src/easy/SearchInsertPosition.java
+++ src/easy/SearchInsertPosition.java
@@ -9,21 +9,13 @@ public class SearchInsertPosition {
         for(int index=0 ; index<numsLen-1 ; index++){
             if(index+1<=numsLen) {
                 if (nums[index] < target && nums[index + 1] >= target) {
-                    System.out.println(nums[index]);
-                    System.out.println(nums[index+1]);
                     return index+1;
-                }else {
-                    System.out.println(nums[index]);
-                    System.out.println(nums[index+1]);
-                    System.out.println("else1");
                 }
             }
             if(nums[numsLen-1]<target){
                 return numsLen;
             }
-
         }
-
       return 1;
     }
 }
