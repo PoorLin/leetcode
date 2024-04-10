a8c664de11fc5a256566877190c96dc46786c26e
jack <s80532s@gmail.com>
2024-04-10 18:29:11 +0800

    <easy> 題目:isSymmetric 修正前 runtime : Runtime 0 ms Beats 100% 修正前 memory :  41.28MB beats 93%

--- src/easy/SymmetricTree.java
+++ src/easy/SymmetricTree.java
@@ -8,17 +8,37 @@ public class SymmetricTree {
     }
     public static boolean isSymmetric(TreeNode root) {
         invertTreeRes(root.left);
-
-
-
-return isSymmetricRec(root.left,root.right);
+return isSymmetricRecRes(root.left,root.right);
     }
-    public static boolean isSymmetricRec(TreeNode leftNode,TreeNode rightNode) {
-        
 
 
+    public static boolean isSymmetricRecRes(TreeNode leftNode,TreeNode rightNode) {
+        if (leftNode==null && rightNode==null){
+            return true;
+        }else if(leftNode!=null && rightNode!=null){
 
-
-        return false;
+            boolean leftBol=isSymmetricRecRes(leftNode.left,rightNode.left);
+            boolean rightBol=isSymmetricRecRes(leftNode.right,rightNode.right);
+            if(!leftBol || !rightBol){
+                return false;
+            }
+            if(leftNode.val != rightNode.val){
+                return false;
+            }else {
+                return true;
+            }
+        }else {
+            return false;
+        }
+    }
+    public static void invertTreeRes(TreeNode root) {
+        if (root==null){
+            return;
+        }
+        invertTreeRes(root.right);
+        invertTreeRes(root.left);
+        TreeNode tmp=root.left;
+        root.left=root.right;
+        root.right=tmp;
     }
 }
