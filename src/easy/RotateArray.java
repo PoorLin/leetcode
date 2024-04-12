465d5e740fc40c6b0ede518d548984065f984507
jack <s80532s@gmail.com>
2024-04-12 23:58:18 +0800

    <med> 題目:RotateArray  runtime : Runtime 1 ms Beats 49.33%  memory :  56.6MB beats 93.84%

---
+++ src/easy/RotateArray.java
@@ -0,0 +1,81 @@
+package easy;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+        rotate(new int[]{1,2,3,4,5,6,7},3);
+    }
+    public static void rotate(int[] nums, int k) {
+        int numsLen = nums.length;
+
+
+
+        if(numsLen<1000) {
+            for (int out = 0; out < k; out++) {
+                int tmp;
+                int after = nums[0];
+                for (int index = 0; index < numsLen; index++) {
+
+                    if (index != numsLen - 1) {
+                        tmp = nums[index + 1];
+                        nums[index + 1] = after;
+                        after = tmp;
+                    } else {
+                        nums[0] = after;
+                    }
+
+                }
+            }
+        }else {
+
+            int[] end=Arrays.copyOfRange(nums,0,k+1);
+            int[] front =Arrays.copyOfRange(nums,numsLen-k,numsLen);
+            int[] array = new int[numsLen];
+       System.arraycopy(front,0,nums,0,k);
+            System.arraycopy(end,0,nums,k,numsLen-k);
+
+
+        }
+
+    }
+//         int numsLen = nums.length;
+//         int count = 0;
+//         int thisNode = nums[0];
+//         int lastNode = nums[0];
+//         int nowIndex=0;
+//         while (count<numsLen){
+//             System.out.println(count);
+////             if(nowIndex == 0){
+////                 tmp=nums[nowIndex+k];
+////                 nums[nowIndex+k]=nums[nowIndex];
+////                 nowIndex+=k;
+////             }
+//             if(nowIndex+k>numsLen-1){
+//                 thisNode=nums[(nowIndex+k)%numsLen];
+//                 nums[(nowIndex+k)%numsLen]=lastNode;
+//                 nowIndex = (nowIndex+k)%numsLen;
+//                 lastNode=thisNode;
+//                 System.out.println(thisNode);
+//                 System.out.println(lastNode);
+//
+//             }else {
+//
+//                 thisNode=nums[nowIndex+k];
+//                 nums[nowIndex+k]=lastNode;
+//                 nowIndex+=k;
+//                 lastNode=thisNode;
+//                 System.out.println(thisNode);
+//                 System.out.println(lastNode);
+//             }
+//
+//
+//
+//             count++;
+//
+//         }
+//
+//
+//    }
+}
