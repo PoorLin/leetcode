64fc4062871aa99d416cb1ae182393bc19598f25
jack <s80532s@gmail.com>
2024-04-12 20:51:03 +0800

    <med> 題目:RemoveDuplicates  Rewrite  runtime : Runtime 0 ms Beats 100%  memory :  44.34MB beats 59.3%

---
+++ src/easy/RemovDuplicatesArrayII.java
@@ -0,0 +1,31 @@
+package easy;
+
+public class RemovDuplicatesArrayII {
+
+
+    public int removeDuplicates(int[] nums) {
+        int nowindex=-1;
+        int nowInt = -101;
+        int numLen = nums.length;
+        int count = 0;
+        for(int index = 0 ; index<numLen;index++){
+            if(nums[index]>nowInt ){  //如果
+                count = 0;
+                nowInt=nums[index];  //
+                nowindex++;
+                nums[nowindex]=nums[index];
+                count++;
+            }else if(nums[index]==nowInt && count<2){
+                nowindex++;
+                nums[nowindex]=nums[index];
+                count++;
+            }else {
+
+            }
+        }
+
+        return nowindex+1;
+    }
+
+
+}
